73fd97242719128077fe254c6a3cd266e27bf075
Yousufzoda Komron <komron.yusufjonovich@gmail.com>
2022-10-31 00:04:43 +0300

    created-migration-user-stories
    completed task 4

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -36,7 +36,114 @@ class CreateCinemaSchema extends Migration
      */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        Schema::create('roles', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('films', function($table) {
+            $table->increments('id');
+            $table->string('title', 64);
+            $table->text('description')->nullable;
+            $table->dateTime('duration');
+            $table->string('language', 16);
+            $table->string('genre');
+            $table->dateTime('release_date');
+            $table->string('country', 64);
+            $table->timestamps();
+        });
+
+        Schema::create('cinema', function($table) {
+            $table->increments('id');
+            $table->string('name', 64);
+            $table->integer('total_showrooms');
+            $table->string('address');
+            $table->string('phone');
+            $table->timestamps();
+        });
+
+        Schema::create('showrooms', function($table) {
+            $table->increments('id');
+            $table->string('name', 64);
+            $table->integer('total_seats');
+            $table->integer('cinema_id')->unsigned();
+            $table->foreign('cinema_id')->references('id')->on('cinema')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('showroom_seat', function($table) {
+            $table->increments('id');
+            $table->integer('seat_number');
+            $table->string('seat_type');
+            $table->integer('showroom_id')->unsigned();
+            $table->foreign('showroom_id')->references('id')->on('showrooms')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+
+        Schema::create('shows', function($table) {
+            $table->increments('id');
+            $table->dateTime('date');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->integer('showroom_id')->unsigned();
+            $table->foreign('showroom_id')->references('id')->on('showrooms')->onDelete('cascade');
+            $table->integer('film_id')->unsigned();
+            $table->foreign('film_id')->references('id')->on('films')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('bookings', function($table) {
+            $table->increments('id');
+            $table->integer('number_of_seat');
+            $table->dateTime('time');
+            $table->integer('status')->default(1);
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('show_id')->unsigned();
+            $table->foreign('show_id')->references('id')->on('shows')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('show_seat', function($table) {
+            $table->increments('id');
+            $table->integer('status')->default(1);
+            $table->float('price', 2);
+            $table->integer('showroom_seat_id')->unsigned();
+            $table->foreign('showroom_seat_id')->references('id')->on('showroom_seat')->onDelete('cascade');
+            $table->integer('show_id')->unsigned();
+            $table->foreign('show_id')->references('id')->on('shows')->onDelete('cascade');
+            $table->integer('booking_id')->unsigned();
+            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('payments', function($table) {
+            $table->increments('id');
+            $table->float('amount', 2);
+            $table->dateTime('date');
+            $table->float('discount');
+            $table->integer('transaction_id');
+            $table->string('payment_method');
+            $table->integer('booking_id')->unsigned();
+            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('tickets', function($table) {
+            $table->increments('id');
+            $table->integer('show_id')->unsigned();
+            $table->foreign('show_id')->references('id')->on('shows')->onDelete('cascade');
+            $table->integer('payment_id')->unsigned();
+            $table->foreign('payment_id')->references('id')->on('payments')->onDelete('cascade');
+            $table->integer('booking_id')->unsigned();
+            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->dateTime('date');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -46,5 +153,15 @@ class CreateCinemaSchema extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('films');
+        Schema::dropIfExists('cinema');
+        Schema::dropIfExists('showrooms');
+        Schema::dropIfExists('showroom_seat');
+        Schema::dropIfExists('shows');
+        Schema::dropIfExists('show_seat');
+        Schema::dropIfExists('bookings');
+        Schema::dropIfExists('payments');
+        Schema::dropIfExists('tickets');
+        Schema::dropIfExists('roles');
     }
 }
